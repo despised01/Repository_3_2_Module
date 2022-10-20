68ee1ac3494f43e554c3c0db704879f7daa5b663
Антон Левин <despised01@gmail.com>
2022-10-20 22:19:36 +0300

    Добавьте файлы проекта.

---
+++ App_3_2_Module/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace App_3_2_Module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Anton";
+            Console.WriteLine(MyName);
+        }
+    }
+}
