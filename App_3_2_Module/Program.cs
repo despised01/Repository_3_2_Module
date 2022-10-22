7c42252d0d5871280949eb5e4b25a7591c755172
Антон Левин <despised01@gmail.com>
2022-10-22 19:42:36 +0300

    Добавлены и обозначены переменные "MyAge", "MyPet", "MySize". Добавлены команды для выведения в консоль. Задание 3.3

--- App_3_2_Module/Program.cs
+++ App_3_2_Module/Program.cs
@@ -7,8 +7,19 @@ namespace App_3_2_Module
         static void Main(string[] args)
         {
             string MyName;
+            byte MyAge;
+            bool MyPet;
+            double MySize;
+
             MyName = "Anton";
-            Console.WriteLine("Привет" + MyName);
+            MyAge = 30;
+            MyPet = true;
+            MySize = 43.5f;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("My age is " + MyAge);
+            Console.WriteLine("Do I have a pet? " + MyPet);
+            Console.WriteLine("My shoe size is " + MySize);
         }
     }
 }
