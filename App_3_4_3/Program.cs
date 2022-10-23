5d7c6be1e36aa8c0394566bd13763fdcce078549
Антон Левин <despised01@gmail.com>
2022-10-23 20:31:59 +0300

    Добавлено приложения к заданию 3.4.3, добавлено перечисление "Semaphore"

---
+++ App_3_4_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace App_3_4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+enum Semaphore
+{
+    Red = 100,
+    Yellow = 200,
+    Green = 300
+}
