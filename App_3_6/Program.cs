0c00108760cbf5d2fbfdb1af7418b964d5b8ae85
Антон Левин <despised01@gmail.com>
2022-10-23 22:15:16 +0300

    Добавлен код к заданию 3.6.8

--- App_3_6/Program.cs
+++ App_3_6/Program.cs
@@ -8,6 +8,11 @@ namespace App_3_6
         {
             double result = 5.0 / 2.0;
             Console.WriteLine("5/2 = {0}", result);
+
+            double result_rem = 10 % 3;
+            Console.WriteLine("10 % 3 = {0}", result_rem);
+
+            Console.ReadKey();
         }
     }
 }
