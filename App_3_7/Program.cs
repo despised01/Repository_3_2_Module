aeb5c14b3c444feb9e7be59302877f561df5d489
Антон Левин <despised01@gmail.com>
2022-10-24 13:08:24 +0300

    переименование репозитория в "App_3_Module", добавлена программа к блоку 3.7

---
+++ App_3_7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace App_3_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            
+            string name = Console.ReadLine();
+           
+            Console.Write("Enter your age: ");
+           
+            byte age = (byte)int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            
+            Console.Write("What is your favorite day of week?");
+            
+            DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Your favorite day is {0}", day);
+        }
+    }
+}
